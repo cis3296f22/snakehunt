3da6c927c8444b6f0c189ec3d578f68f5fd0515e
Katrina Janeczko <73796086+katrinajaneczko@users.noreply.github.com>
2022-10-31 21:56:03 -0400

    change server.py
    also sadly not working, but testing out some incomplete framework for oop multithreading

--- server.py
+++ server.py
@@ -1,74 +1,78 @@
 import socket
-from _thread import *
+import threading #referenced https://realpython.com/intro-to-python-threading/
+import pickle
 import sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server = "localhost"
-port = 5555
-playerCount = 0
-
-try:
-    s.bind((server, port))
-except socket.error as e:
-    str(e)
-
-#listen/wait for incoming client connections
-s.listen(2)
-print("Waiting for a connection, Server Started")
-
-#convert string "(x,y)" to int tuple (x,y)
-def read_pos(str): 
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-#convert int tuple (x,y) to string "(x,y)"
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-#initial positions of 3 players; pos[0]=(0,0)
-pos = [(0,0),(100,100)]#,(0,100)]
-
-#connect every client from addresses provided by server simultaneously
-def threaded_client(conn, player):
-    #send data, convert (0,0) to "(0,0)"
-    conn.send(str.encode(make_pos(pos[player])))
-    reply = ""
-    while True:
-        '''conn.recv gets data from every client
-        & returns server response to a specific client
-        '''
-        try:
-            #read received data and convert "(0,0)" to (0,0)
-            data = read_pos(conn.recv(2048).decode()) #(int,int)
-            #set player position to (0,0) [player1]
-            pos[player] = data
-
-            #if data not received, 
-            if not data:
-                print("Disconnected")
+class Server:
+    def __init__(self):
+        self.running = True
+        
+    def Session(self, conn_list):
+        player1_data = []
+        player2_data = []
+        
+        while self.running:
+            player1_data = conn_list[0].recv(1024)
+            player2_data = conn_list[1].recv(1024)
+            if not (player1_data or player2_data):
                 break
-            else:
-                #if player1, reply with player0 pos
-                if player == 1:
-                    reply = pos[0]
-                #if player0, reply with player1 pos
-                else:
-                    reply = pos[1]
             
-            #send reply
-            conn.sendall(str.encode(make_pos(reply)))
-                    
-                    
-        except:
-            break
+            player1_data = pickle.loads(player1_data)
+            player2_data = pickle.loads(player2_data)
+            player1_data = pickle.dumps(player1_data)
+            player2_data = pickle.dumps(player2_data)
+            
+            conn_list[0].sendall(player1_data)
+            conn_list[1].sendall(player2_data)
+            
+            
+    def Start(self):
+        PLAYERCOUNT = 2
+        self.running = True
+        HOST = "localhost"
+        PORT = 5556
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.s.bind((HOST, PORT))
+        except socket.error as e:
+            str(e)
 
-    print("Lost connection")
-    conn.close()
+        #playerCount = 0 ###?
 
-playerCount = 0
-while True:
-    conn, addr = s.accept()
-    print("Connected to:", addr)
-    start_new_thread(threaded_client, (conn, playerCount))
-    playerCount += 1
-s.close()
+        #listen/wait for incoming client connections
+        self.s.listen(5)
+        conn_list = []
+        threads = []
+        print("Waiting for a connection, Server Started")
+        
+        while self.running:
+            for i in range(PLAYERCOUNT):
+                try:
+                    if i:
+                        print("Waiting for Player {} to connect".format(i))
+                    conn, addr = self.s.accept()
+                    print("Connected to:", addr)
+                    conn_list.append(conn) #add new connection to list of connections
+                except:
+                    self.running = False
+            
+            threads.append(threading.Thread(target=self.Session, args=(conn_list,)))
+            threads[-1].start() #start just-added thread
+            conn_list = []
+            
+    def End(self):
+        self.s.close()
+        self.running = False
+        
+class Window():
+    def __init__(self):
+        self.server_online = False
+        self.new_server = Server()
+        if not self.server_online:
+            self.server = threading.Thread(target=self.new_server.Start, args=())
+            self.server.start()
+            self.server_online = True
+        else:
+            print("Server running already.")
+if __name__ == "__main__":
+    window = Window()
